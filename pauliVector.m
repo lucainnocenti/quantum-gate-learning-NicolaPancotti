5762fe39a8293b6799bf971a3f1092b98553dc56
nicaiola <nicola.pancotti@gmail.com>
2015-05-30 12:08:30 +0200

    deleteML

--- pauliVector.m
+++
@@ -1,8 +0,0 @@
-function [P] = pauliVector()
-    
-    sx=[0,1;1,0]; sy=[0,-1i;1i,0]; sz=[1,0;0,-1]; id=eye(2);
-    P = cell(1,4);
-    P{1} = id; P{2} = sx; P{3} = sy; P{4} = sz; 
-
-end
-
--- updateCleft.m
+++
@@ -1,7 +0,0 @@
-function [Cleft]=updateCleft(Cleft,B,X,A)
-    % see comment in updateCright.m 
-    if isempty(X), X=reshape(eye(size(B,3)),[1,1,2,2]); end
-    Cleft=contracttensors(A,3,1,Cleft,3,3);
-    Cleft=contracttensors(X,4,[1,4],Cleft,4,[4,2]);
-    Cleft=contracttensors(conj(B),3,[1,3],Cleft,4,[4,2]);
-end
